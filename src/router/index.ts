99795ba1b05dfe275dc85d7db53904228c3ce289
jabbalh <nicolas.huet35@gmail.com>
2026-05-20 15:25:12 +0200

    Maj

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/HomeView.vue'
 import About from '@/views/AboutView.vue'
 import Gallery from '@/views/GalleryView.vue'
@@ -6,7 +6,7 @@ import Events from '@/views/EventsView.vue'
 import Contact from '@/views/ContactView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: Home },
     { path: '/association', component: About },
